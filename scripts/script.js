7a95040b1929b0f13a959fe33eb66ec1369767d1
btisoniah <93206396+btisoniah@users.noreply.github.com>
2021-11-08 22:34:01 -0300

    inicio do projeto

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,17 @@
-const contador = document.getElementById('contador')
+const elementoLista = document.querySelector('ul'); 
+const elementoInput = document.querySelector('input');
+const elementoBotao = document.querySelector('button'); 
 
-contador.addEventListener('click', event =>  {
-    event.target.innerHTML++
-}) 
+const listaTarefas = ['teste']; 
+
+function exibirTarefas (){
+    for (let i=0; listaTarefas.length)
+    const elementoTarefa = document.createElement('li')
+    const textoTarefa = document.createTextNode('tarefa')
+}
+
+function adicionarTarefa(){ 
+const textoTarefa = elementoInput.value
+listaTarefas.push(textoTarefa)
+elementoInput.value = ''; //limpa o imput após adicionar tarefa 
+}
