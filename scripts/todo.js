fd0797784dab2a4208f219f5d3db6a55b1afdedc
Sonia Hentz <93206396+btisoniah@users.noreply.github.com>
2021-11-10 17:02:46 -0300

    tentativas funcao js

--- scripts/todo.js
+++ scripts/todo.js
@@ -1,19 +1,15 @@
- //var elementoLista = document.querySelector('ul');
-// var elementoInput = document.querySelector('input');
-// var elementoBotao = document.querySelector('button');
 
-function incluirElemenetoNaLista() {
- 
-  var inserirValor = document.getElementById("novaTarefa").value //valor que for digitado na caixa... 
-  var li = document.createElement("li");
-  var x = document.createTextNode(inserirValor);
+function addElemento() {
+var inserirValor = document.getElementById("novaTarefa").value //valor que for digitado na caixa... 
+var li = document.createElement("li");
+var x = document.createTextNode(inserirValor);
+
   li.appendChild(x); 
   document.getElementById("lista").appendChild(li);
   document.getElementById("novaTarefa").value = ""; 
-  console.log(inserirValor)
   
-   for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
+   for (i = 0; i < lista.length; i++) {
+    lista[i].onclick = function() {
       var div = this.parentElement;
       div.style.display = "none";
     }
